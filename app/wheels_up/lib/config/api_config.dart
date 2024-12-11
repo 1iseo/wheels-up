cdd6b4b5029e7935b7e0ef8bda0d3cb84e442875
1iseo <96890370+1iseo@users.noreply.github.com>
2024-12-11 12:16:10 +0700

    remove config before commit

--- app/wheels_up/lib/config/api_config.dart
+++ app/wheels_up/lib/config/api_config.dart
@@ -1,4 +1,4 @@
 class ApiConfig {
   static const String baseUrl = 'http://localhost:3333';  // Update this with your actual backend URL
-  static const String pocketbaseUrl = 'http://3.1.221.129:8080/';
+  static const String pocketbaseUrl = '';
 }
